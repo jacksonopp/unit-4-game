38466ef0a5603a31d9d42a6f87333462c25c01cd
Jackson Oppenheim <jacksonopp@gmail.com>
2019-08-03 09:55:35 -0500

    added gems, ability to tell which gem you click on, and store a random number as it's 'value' attribute. Also uploading the code my cat typed for posterity

---
+++ assets/javascript/game.js
@@ -0,0 +1,47 @@
+let goal = 0;
+let score = 0;
+let wins = 0;
+let losses = 0;
+let crystalValue = 0;
+
+let winCon = false;
+let loseCon = false;
+
+function randomGoal() {
+    goal = Math.floor(Math.random() * 100 + 20);
+}
+
+randomGoal();
+console.log(goal);
+
+function randomCrystalValue() {
+    const x = Math.floor(Math.random() * 12 + 1);
+    return x;
+}
+
+
+document.querySelectorAll(".crystal").forEach(function (crystalEl) {
+    crystalValue = randomCrystalValue();
+    crystalEl.setAttribute("value", crystalValue);
+    // crystalScore = crystalEl.getAttribute("value"); ------------ why doesn't this work
+
+    if (!winCon || !loseCon) {
+        crystalEl.addEventListener("click", function () {
+            crystalNumber = crystalEl.getAttribute("id");
+            console.log("You clicked on: " + crystalNumber);
+            console.log(crystalNumber + "'s value is: " + crystalEl.getAttribute("value"));
+            score += parseInt(crystalEl.getAttribute("value"));
+            console.log(score);
+            //checking for win condition
+            if (score === goal) {
+                console.log("You win!")
+                winCon = true;
+                console.log(winCon);
+            } else if (score > goal) {
+                console.log("You lose!")
+                loseCon = true;
+                console.log(loseCon);
+            }
+        })
+    }
+}) 
